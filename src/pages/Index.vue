0e7e3fc914ce56ffd55fb6d9638199f607a7000f
sebo <contact@sebastiansimko.sk>
2019-11-27 15:25:06 +0100

    improve of mobile view of page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,11 +2,11 @@
   <Layout>
 
     <div class="lg:p-20 md:p-10 p-5">
-      <p class="text-3xl lg:pl-20 font-light md:pl-10 pl-5">
+      <p class="text-3xl lg:pl-20 font-light md:pl-10 pl-5 text-center md:text-left">
         I'm experienced full-stack web developer.
       </p>
-      <div class="flex pt-10">
-        <div class="flex-1 lg:pl-20 leading-loose md:pl-10 pl-5">
+      <div class="flex flex-wrap pt-10">
+        <div class="w-full md:w-1/2 lg:pl-20 leading-loose md:pl-10 text-center md:text-left pb-10 md:pb-0">
           <h4 class="text-uppercase text-gray-400 text-sm pb-5 tracking-widest-xl">NETWORK</h4>
           <ul class="text-sm">
             <li><a href="mailto:contact@sebastiansimko.sk" >contact@sebastiansimko.sk</a></li>
@@ -15,7 +15,7 @@
             <li><a href="https://twitter.com/sebastiansimko">Twitter</a></li>
           </ul>
         </div>
-        <div class="flex-1 text-right lg:pr-20 leading-loose md:pr-10 pr-5">
+        <div class="w-full md:w-1/2 text-center md:text-right lg:pr-20 leading-loose md:pr-10">
           <h4 class="text-uppercase text-gray-400 text-sm pb-5 tracking-widest-xl">SKILLS & FOCUS</h4>
           <ul class="text-sm">
             <li>Javascript in modern application</li>
@@ -28,8 +28,8 @@
         </div>
       </div>
 
-      <div class="flex pt-10">
-        <div class="flex-1 lg:pl-20 leading-loose md:pl-10 pl-5">
+      <div class="flex flex-wrap pt-10">
+        <div class="w-full md:w-1/2 text-center md:text-left lg:pl-20 leading-loose md:pl-10 pb-10 md:pb-0">
           <h4 class="text-uppercase text-gray-400 text-sm pb-5 tracking-widest-xl">EXPERIENCE</h4>
           <ul class="text-sm">
             <li><a href="https://en.wikipedia.org/wiki/Opera_Solutions" target="_blank">Opera solution</a></li>
@@ -39,7 +39,7 @@
             <li><a href="https://adaptiware.company" target="_blank">Adaptiware</a></li>
           </ul>
         </div>
-        <div class="flex-1 text-right lg:pr-20 leading-loose md:pr-10 pr-5">
+        <div class="w-full md:w-1/2 text-center md:text-right lg:pr-20 leading-loose md:pr-10">
           <h4 class="text-uppercase text-gray-400 text-sm pb-5 tracking-widest-xl">GOALS</h4>
           <ul class="text-sm">
             <li>Master the known languages</li>
